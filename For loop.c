e89484085a90e1c79ccc4d548d9e084d2b9cbdc4
minaruth <nguurinelson@gmail.com>
2024-10-11 13:39:00 +0300

    Update

---
+++ For loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0, sumOfSquares = 0;
+
+    for (int i = 1; i <= 50; i++) {
+        sum += i; 
+
+        sumOfSquares += i * i;  
+        printf("%d\t%d\n", i, i * i);  
+    }
+
+    printf("\nSum of numbers from 1 to 50: %d\n", sum);
+    printf("Sum of squares of numbers from 1 to 50: %d\n", sumOfSquares);
+
+    return 0;
+;}
