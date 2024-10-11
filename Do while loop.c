2c9291d6ecd4a9d28ec4ae35703afd70464bcaef
minaruth <nguurinelson@gmail.com>
2024-10-11 13:38:09 +0300

    Do while loop.c

---
+++ Do while loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1, sum = 0, sumOfSquares = 0;
+
+    do {
+        sum += i; 
+        sumOfSquares += i * i; 
+
+        printf("%d\t%d\n", i, i * i); 
+
+        i++;
+    } while (i <= 50);
+
+    printf("\nSum of numbers from 1 to 50: %d\n", sum);
+    printf("Sum of squares of numbers from 1 to 50: %d\n", sumOfSquares);
+
+    return 0;
+}
