db3f8b128c5d4a63fc9c57f37ece27e0e7ab4a96
minaruth <nguurinelson@gmail.com>
2024-10-18 18:15:01 +0300

    Create 1D array.c

---
+++ 1D array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char institute[]={'k','i','r','i','n','y','a','g','a','\0'};
+
+    int j=0;
+
+    while (institute[j]){
+
+    printf("%c",institute[j++]);
+
+}
+    return 0;
+}
